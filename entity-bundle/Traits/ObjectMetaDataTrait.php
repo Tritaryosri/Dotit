ab3e5265d0a994df03e2606a8feb7187e0596a23
Yosri Triter <yosri.triter@dotit-medu.com>
2020-03-10 09:45:22 +0100

    commit

---
+++ entity-bundle/Traits/ObjectMetaDataTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Dotit\EntityBundle\Traits;
+
+/**
+ * Trait ObjectMetaDataTrait
+
+ * @author Tritar Yosri
+ */
+trait ObjectMetaDataTrait
+{
+    /**
+     * Getting object short class name
+     *
+     * @return string
+     * @throws \ReflectionException
+     */
+    public function getClass()
+    {
+        return (new \ReflectionClass($this))->getShortName();
+    }
+}
